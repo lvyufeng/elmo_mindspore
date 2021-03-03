16638772bee96ac8452d25e148021a727fd99e40
lvyufeng <lvyufeng@cqu.edu.cn>
2021-03-03 15:56:59 +0800

    add highway module and modified nn layers which are initialized as same as pytorch

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -3,6 +3,6 @@ DATA_URL=http://www.statmt.org/lm-benchmark/1-billion-word-language-modeling-ben
 wget --no-check-certificate ${DATA_URL}
 
 tar xvf 1-billion-word-language-modeling-benchmark-r13output.tar.gz
-mv 1-billion-word-language-modeling-benchmark-r13output/ data/
+mv 1-billion-word-language-modeling-benchmark-r13output/ dataset/
 
 /bin/rm 1-billion-word-language-modeling-benchmark-r13output.tar.gz
